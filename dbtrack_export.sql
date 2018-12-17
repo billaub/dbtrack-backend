ae93c26efa6ac0876152589a77119165be338973
root <root@ip-172-31-49-153.ec2.internal>
2018-12-17 15:50:40 +0000

    updated export

--- dbtrack_export.sql
+++ dbtrack_export.sql
@@ -20,7 +20,7 @@ SET row_security = off;
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
-
+CREATE DATABASE dbtrack;
 --
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
